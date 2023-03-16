3c12820f609fbe174536bd1f468a42f2cfa2e193
Vadimcha <BelovBadim2014@yandex.ru>
2023-03-16 23:03:33 +0300

    Admin panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import './app.sass'
+import Content from "./components/Content";
+import Table from "./components/Table";
+import { CContainer } from '@coreui/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <Sidebar />
+        <Content>
+            <CContainer className={"workers"}>
+                <h1>Сотрудники</h1>
+                <a href="" >Добавить</a>
+            </CContainer>
+            <Table></Table>
+        </Content>
     </div>
   );
 }
