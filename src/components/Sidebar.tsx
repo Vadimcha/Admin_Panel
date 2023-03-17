9e56bf286588c9ddf8b9312be567e313b15a1359
Vadimcha <BelovBadim2014@yandex.ru>
2023-03-17 09:28:04 +0300

    Add active nav-link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,11 +17,11 @@ const Sidebar = () => {
     return (
         <CSidebar style={{height: "100vh", paddingTop: "10vh", backgroundColor: "#212529", borderRight: "1px solid #4e4e4e"}}  unfoldable>
             <CSidebarNav>
-                <CNavItem href="#">
+                <CNavItem href="#" active={activ==="Problems"} onClick={() => setActiv("Problems")}>
                     <CIcon customClassName="nav-icon" icon={cilDescription} style={{fontWeight: "bold"}} />
                     <p style={{ fontWeight: "bold", margin: "0", padding: "0" }}>Задачи</p>
                 </CNavItem>
-                <CNavItem href="#" active={activ==="Worker"}>
+                <CNavItem href="#" active={activ==="Worker"} onClick={() => setActiv("Worker")}>
                     <CIcon customClassName="nav-icon" icon={cilGroup} style={{fontWeight: "bold"}} />
                     <p style={{ fontWeight: "bold", margin: "0", padding: "0" }}>Сотрудники</p>
                 </CNavItem>
